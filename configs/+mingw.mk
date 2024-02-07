0dd9c2fbdb4b029d450b6de24c3e978811d485f9
Zh3k4 <henz20131@gmail.com>
2024-02-07 23:14:17 +0300

    use paths of pre-built binaries

--- configs/+mingw.mk
+++ configs/+mingw.mk
@@ -1,6 +1,6 @@
 CC = x86_64-w64-mingw32-gcc
-INCLUDE = -Iraylib-mingw/include
-LIBS = -Lraylib-mingw/lib -lraylib -lgdi32 -lwinmm
+INCLUDE = -Iraylib-5.0_win64_mingw-w64/include
+LIBS = -Lraylib-5.0_win64_mingw-w64/lib -lraylib -lgdi32 -lwinmm
 
 TARGET = skin-view.exe
 
