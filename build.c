ae005de42c3ae66ee7e41ad84cdba9d5cdf254b4
Zh3k4 <henz20131@gmail.com>
2024-06-03 21:18:43 +0300

    add c-compiler-only build option

---
+++ build.c
@@ -0,0 +1,158 @@
+#include "build.h"
+
+#define CFLAGS \
+	"--std=c11", "-pedantic", \
+	"-Os", \
+	"-Wall", "-Wextra", "-Wshadow", "-Wconversion", "-Werror", \
+	"-D_XOPEN_SOURCE=700", \
+	"-DVERSION=\"0.4.0\""
+#define INCLUDE "-Iraylib/src"
+#define LDFLAGS "-s"
+
+#ifdef _WIN32
+#	define TARGET "skin-view.exe"
+#	define BUNDLE "src/bundle.exe"
+#	define LIBS "-Lraylib/src/", "-lraylib", "-lgdi32", "-lwinmm"
+#else
+#	define TARGET "skin-view"
+#	define BUNDLE "src/bundle"
+#	define LIBS "-Lraylib/src/", "-lraylib", "-lm"
+#endif
+
+const char *const obj[] = {
+	"src/main.o",
+	NULL,
+};
+
+i8
+make(char *prefix, char *output, char **deps, char **cmd)
+{
+	i8Result r = needs_rebuild(output, (const char**)deps);
+	if (!r.ok) return 0;
+	if (!r.val) return 1;
+
+	Proc p = create_process(cmd);
+	if (p == INVALID_PROC) return 0;
+	if (!wait_for_process(p)) return 0;
+	printf("%s\t%s\n", prefix, output);
+	return 1;
+}
+
+i8
+c2o(char *ofile)
+{
+	assert(ofile);
+
+	char *cfile = strdup(ofile);
+	assert(cfile);
+
+	cfile[strlen(cfile) - 1] = 'c';
+
+	i8 status = make("CC", ofile, (char *[]){ cfile, NULL }, (char *[]){
+			CC, CFLAGS, INCLUDE, "-c", cfile, "-o", ofile, NULL
+		});
+
+	free(ofile);
+	return status;
+}
+
+i8
+skin_view(void)
+{
+	return make("CCLD", TARGET, (char *[]){ "src/main.o", NULL },
+		(char *[]){
+			CC, LDFLAGS, "src/main.o", "-o", TARGET, LIBS, NULL
+		});
+}
+
+i8
+build(void)
+{
+	i8 status = 1;
+	{
+		char *deps[] = { "src/bundle.c", NULL };
+		char *cmd[] = {
+			CC, CFLAGS, LDFLAGS, INCLUDE, "src/bundle.c", "-o", BUNDLE, NULL
+		};
+		status = status && make("CCLD", BUNDLE, deps, cmd);
+	}
+	{
+		char *deps[] = { BUNDLE, NULL };
+		char *cmd[] = { BUNDLE, NULL };
+		status = status && make("BUNDLE", "src/bundle.h", deps, cmd);
+	}
+	{
+		char *deps[] = { "src/main.c", "src/bundle.h", NULL };
+		char *cmd[] = { CC, CFLAGS, INCLUDE, "-c", "src/main.c", "-o", "src/main.o", NULL };
+		status = status && make("CC", "src/main.o", deps, cmd);
+	}
+	return status && skin_view();
+}
+
+i8
+remove_(const char *pathname)
+{
+	assert(pathname);
+	errno = 0;
+	if (remove(pathname) && errno != ENOENT) return 0;
+	if (errno != ENOENT) printf("RM\t%s\n", pathname);
+	return 1;
+}
+
+i8
+clean(void)
+{
+	i8 status = remove_(TARGET)
+		&& remove_(BUNDLE)
+		&& remove_("src/bundle.h");
+
+	for (usize i = 0; obj[i]; i += 1)
+		status = status && remove_(obj[i]);
+
+	return status;
+}
+
+enum Job {
+	JOB_NONE,
+	JOB_BUILD,
+	JOB_CLEAN,
+	JOB_DIST,
+};
+
+void
+usage(int err)
+{
+	fprintf(err ? stderr : stdout,
+		"Usage: build <build|clean|dist>\n");
+}
+
+int
+main(int argc, char **argv)
+{
+	enum Job j = JOB_NONE;
+	if (argc == 2 && !strcmp(argv[1], "help")) {
+		usage(0);
+		return 0;
+	}
+	else if (argc == 1 || (argc == 2 && !strcmp(argv[1], "build"))) {
+		j = JOB_BUILD;
+	}
+	else if (argc == 2 && !strcmp(argv[1], "clean")) {
+		j = JOB_CLEAN;
+	}
+	else if (argc == 2 && !strcmp(argv[1], "dist")) {
+		j = JOB_DIST;
+	}
+
+	switch (j) {
+	case JOB_BUILD:
+		return !build();
+	case JOB_CLEAN:
+		return !clean();
+	case JOB_DIST: assert(0 && "not implemented");
+	default:
+		usage(1);
+		return 1;
+	}
+	return 0;
+}
---
+++ build.h
@@ -0,0 +1,287 @@
+#ifndef _WIN32
+#	define _POSIX_C_SOURCE 200809L
+#endif
+#include <assert.h>
+#include <errno.h>
+#include <inttypes.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifdef _WIN32
+#	define WIN32_LEAN_AND_MEAN
+#	define _WINUSER_
+#	define _WINGDI_
+#	define _IMM_
+#	define _WINCON_
+#	include <windows.h>
+#	include <direct.h>
+#	include <shellapi.h>
+#else
+#	include <sys/wait.h>
+#	include <sys/stat.h>
+#	include <unistd.h>
+#endif
+
+typedef uint8_t   u8;
+typedef int8_t    i8;
+typedef uint16_t  u16;
+typedef int16_t   i16;
+typedef uint32_t  u32;
+typedef int32_t   i32;
+typedef uint64_t  u64;
+typedef int64_t   i64;
+typedef uintptr_t uptr;
+typedef ptrdiff_t ptrdiff;
+typedef size_t    usize;
+
+#define countof(a)  (sizeof(a)/sizeof(*(a)))
+#define lengthof(s) (countof(s) - 1)
+
+#define foreach(o, a) \
+	for (usize i = 0; i < countof(a); i += 1) { \
+		o = a[i];
+
+typedef struct {
+	i8 ok;
+	i8 val;
+} i8Result;
+
+#ifdef _WIN32
+#	if defined(__GNUC__)
+#		define CC "echo"
+#	elif defined(__clang__)
+#		define CC "clang"
+#	elif defined(_MSC_VER)
+#		define CC "cl"
+#	endif
+#else
+#	define CC "cc"
+#endif
+
+#ifdef _WIN32
+	typedef HANDLE Proc;
+#	define INVALID_PROC INVALID_HANDLE_VALUE
+#else
+	typedef int Proc;
+#	define INVALID_PROC (-1)
+#endif
+
+char *
+vec2str(char *vec[])
+{
+	char *string = malloc(1);
+	usize sz = 0, i = 0;
+	do {
+		char *arg = vec[i];
+		usize len = strlen(arg);
+
+		i8 has_specials = strchr(arg, ' ') || strchr(arg, '"');
+
+		char *tmp = realloc(string,
+			(sz + len + (has_specials ? 3 : 1))*sizeof(char));
+		assert(tmp);
+		string = tmp;
+
+		if (has_specials) {
+			string[sz] = '\'';
+			sz += 1;
+		}
+		strncpy(&string[sz], arg, len);
+		if (has_specials) {
+			string[sz + len] = '\'';
+			sz += 1;
+		}
+		string[sz + len] = ' ';
+		sz += len + 1;
+
+		i += 1;
+	} while (vec[i]);
+	string[sz] = '\0';
+
+	return string;
+}
+
+Proc
+create_process(char *command[])
+{
+#ifdef _WIN32
+
+	/* https://docs.microsoft.com/en-us/windows/win32/procthread/creating-a-child-process-with-redirected-input-and-output */
+	STARTUPINFO si;
+	ZeroMemory(&si, sizeof(si));
+	si.cb = sizeof(STARTUPINFO);
+	// NOTE: theoretically setting NULL to std handles should not be a problem
+	// https://docs.microsoft.com/en-us/windows/console/getstdhandle?redirectedfrom=MSDN#attachdetach-behavior
+	// TODO: check for errors in GetStdHandle
+	si.hStdError = GetStdHandle(STD_ERROR_HANDLE);
+	si.hStdOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+	si.hStdInput = GetStdHandle(STD_INPUT_HANDLE);
+	si.dwFlags |= STARTF_USESTDHANDLES;
+
+	PROCESS_INFORMATION pi;
+	ZeroMemory(&pi, sizeof(pi));
+
+	char *cmd = vec2str(command);
+	/* printf("ZZZZZZZZZ --------- :%s\n", cmd); */
+	/* FIXME: does this even work on win? Cannot test on linux */
+	BOOL ok = CreateProcessA(
+		NULL, cmd, NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi);
+	free(cmd);
+
+	if (!ok) {
+		fprintf(stderr, "Err: Could not create child process: %lu\n", GetLastError());
+		return INVALID_PROC;
+	}
+
+	CloseHandle(pi.hThread);
+	return pi.hProcess;
+
+#else
+
+	pid_t pid = fork();
+	if (pid < 0) {
+		fprintf(stderr, "Err: Could not fork process: %s\n", strerror(errno));
+		return INVALID_PROC;
+	}
+
+	if (pid > 0) return pid;
+
+	if (execvp(command[0], command) < 0) {
+		fprintf(stderr, "Err: Could not exec process: %s\n", strerror(errno));
+		exit(1);
+	}
+
+	assert(0 && "unreachable");
+#endif /* _WIN32 */
+}
+
+i8
+wait_for_process(Proc proc)
+{
+	if (proc == INVALID_PROC) return 0;
+
+#ifdef _WIN32
+
+	DWORD result = WaitForSingleObject(proc, INFINITE);
+	if (result == WAIT_FAILED) {
+		fprintf(stderr, "Err: could not wait on process: %lu\n", GetLastError());
+		return 0;
+	}
+
+	DWORD exit_status;
+	if (!GetExitCodeProcess(proc, &exit_status)) {
+		fprintf(stderr, "Err: could not get process exit code: %lu\n",
+			GetLastError());
+		return 0;
+	}
+
+	if (exit_status != 0) {
+		fprintf(stderr, "Err: command exited with exit code %lu\n",
+			exit_status);
+		return 0;
+	}
+
+	CloseHandle(proc);
+	return 1;
+
+#else
+
+	for (;;) {
+		int wstatus = 0;
+		if (waitpid(proc, &wstatus, 0) < 0) {
+			fprintf(stderr, "Err: could not wait on command (pid %d): %s\n",
+				proc, strerror(errno));
+			return 0;
+		}
+
+		if (WIFEXITED(wstatus)) {
+			int exit_status = WEXITSTATUS(wstatus);
+			if (exit_status != 0) {
+				fprintf(stderr, "Err: command exited with exit code %d\n",
+					exit_status);
+				return 0;
+			}
+			break;
+		}
+
+		if (WIFSIGNALED(wstatus)) {
+			fprintf(stderr, "Err: command was terminated by %s\n",
+				strsignal(WTERMSIG(wstatus)));
+			return 0;
+		}
+	}
+	return 1;
+
+#endif
+}
+
+i8Result
+needs_rebuild(const char *output, const char **inputs)
+{
+#ifdef _WIN32
+	BOOL success;
+	HANDLE outfd = CreateFile(output, GENERIC_READ, 0, NULL,
+		OPEN_EXISTING, FILE_ATTRIBUTE_READONLY, NULL);
+	if (outfd == INVALID_HANDLE_VALUE) {
+		if (GetLastError() == ERROR_FILE_NOT_FOUND) {
+			return (i8Result){ .ok = 1, .val = 1 };
+		}
+		fprintf(stderr, "Err: could not open file %s: %lu", output, GetLastError());
+		return (i8Result){ .ok = 0 };
+	}
+	FILETIME outtime;
+	success = GetFileTime(outfd, NULL, NULL, &outtime);
+	CloseHandle(outfd);
+	if (!success) {
+		fprintf(stderr, "Err: Could not get time of %s: %lu", output, GetLastError());
+		return (i8Result){ .ok = 0 };
+	}
+
+	for (usize i = 0; inputs[i]; i += 1) {
+		const char *input = inputs[i];
+		HANDLE infd = CreateFile(input, GENERIC_READ, 0, NULL,
+			OPEN_EXISTING, FILE_ATTRIBUTE_READONLY, NULL);
+		if (infd == INVALID_HANDLE_VALUE) {
+			fprintf(stderr, "Err: could not open file %s: %lu", input, GetLastError());
+			return (i8Result){ .ok = 0 };
+		}
+		FILETIME intime;
+		success = GetFileTime(infd, NULL, NULL, &intime);
+		CloseHandle(infd);
+		if (!success) {
+			fprintf(stderr, "Err: Could not get time of %s: %lu", input, GetLastError());
+			return (i8Result){ .ok = 0 };
+		}
+
+		if (CompareFileTime(&intime, &outtime) > 0)
+			return (i8Result){ .ok = 1, .val = 1 };
+	}
+
+	return (i8Result){ .ok = 1, .val = 0 };
+
+#else
+
+	struct stat st = {0};
+	if (stat(output, &st) < 0) {
+		if (errno == ENOENT) return (i8Result){ .ok = 1, .val = 1 };
+		fprintf(stderr, "Err: could not stat %s: %s\n", output, strerror(errno));
+		return (i8Result){ .ok = 0 };
+	}
+	int outtime = st.st_mtime;
+
+	for (usize i = 0; inputs[i]; i += 1) {
+		const char *input = inputs[i];
+		if (stat(input, &st) < 0) {
+			fprintf(stderr, "Err: could not stat %s: %s\n", input, strerror(errno));
+			return (i8Result){ .ok = 0 };
+		}
+		int intime = st.st_mtime;
+		if (intime > outtime) return (i8Result){ .ok = 1, .val = 1 };
+	}
+
+	return (i8Result){ .ok = 1, .val = 0 };
+
+#endif
+}
