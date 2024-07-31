7be00f31f10a52d4a4b24eccae8488e1ec14e817
Zh3k4 <henz20131@gmail.com>
2024-07-31 14:44:00 +0300

    chore: fix typo

--- build.c
+++ build.c
@@ -8,7 +8,7 @@ const char *const cflags[] = {
 };
 const char *const libs[] = {
 	"/LIBPATH:raylib/lib/x86_64-w64-msvc",
-	"raylib.lib", "gdi32.lib", "winmm.lib", "user32.lib" "shell32.lib", 0
+	"raylib.lib", "gdi32.lib", "winmm.lib", "user32.lib", "shell32.lib", 0
 };
 
 const char *const include[] = { "/Iraylib/include", 0 };
